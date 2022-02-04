d68fdb3d94a999e659fe63f5771b93ee955b9b4b
Alanov <ghstng@protonmail.ch>
2022-02-04 14:29:42 -0300

    added css variables

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const container = document.querySelector("#container");
 
 function createDivs() {
+    let size = 16;
+    let htmlStyles = window.getComputedStyle(document.querySelector("html"));
+    //let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
+    //let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
     const squaresDiv = document.createElement("div");
-    for(i = 0; i < 256; i++) {
+    for(i = 0; i < (size*size); i++) {
         const squaresDiv = document.createElement("div");
         squaresDiv.classList.add("squaresDiv");
         //squaresDiv.textContent = "A";
         container.appendChild(squaresDiv);
     }
+    document.documentElement.style.setProperty("--rowNum", size);
+    document.documentElement.style.setProperty("--colNum", size);
     return squaresDiv;
 }
 createDivs();
