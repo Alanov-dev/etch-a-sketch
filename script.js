418fc6bf3e693d72a641017da527f566dd3f6190
Alanov <ghstng@protonmail.ch>
2022-02-04 18:41:15 -0300

    added clear button and resize functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+const header = document.querySelector("#header");
 
 function createDivs() {
     let size = 16;
@@ -21,3 +22,41 @@ function createDivs() {
     return squaresDiv;
 }
 createDivs();
+
+function reset() {
+    const button = document.createElement("button");
+    button.textContent = "Clear";
+    button.addEventListener("click", () => {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    let size = prompt("Select a number, max 100, if the number is above 100, it will default to 16.");
+    if(size <= 100) {
+        for(i = 0; i < (size*size); i++) {
+            const squaresDiv = document.createElement("div");
+            squaresDiv.classList.add("squaresDiv");
+            //squaresDiv.textContent = "A";
+            squaresDiv.addEventListener("mouseenter", () => {
+                squaresDiv.style.backgroundColor = "black";
+            });
+            container.appendChild(squaresDiv);
+        }
+    } else {
+        size = 16;
+        for(i = 0; i < (size*size); i++) {
+            const squaresDiv = document.createElement("div");
+            squaresDiv.classList.add("squaresDiv");
+            //squaresDiv.textContent = "A";
+            squaresDiv.addEventListener("mouseenter", () => {
+                squaresDiv.style.backgroundColor = "black";
+            });
+            container.appendChild(squaresDiv);
+        }
+    }
+
+    document.documentElement.style.setProperty("--rowNum", size);
+    document.documentElement.style.setProperty("--colNum", size);
+    });
+    header.appendChild(button);
+}
+reset();
